aa8ea05d4fa9902b9dc353779f7852c1f09fe649
donovan <donoriviere@gmail.com>
2022-10-12 14:22:02 +0200

    init

---
+++ pierre.js
@@ -0,0 +1,20 @@
+// Créer une fonction appelée getComputerChoice qui renverra au hasard soit 'Pierre', 'Papier' ou 'Ciseaux'.
+// Nous utiliserons cette fonction dans le jeu pour retourner le choix de l'ordinateur
+console.log("hello");
+// Écrire une fonction qui joue un seul tour de Pierre Papier Ciseaux. La fonction doit prendre deux paramètres - playerSelection et computerSelection - puis renvoyer une chaîne qui déclare le vainqueur du tour comme suit : "Vous avez perdu! Le papier bat la pierre"
+
+// Rendre le paramètre playerSelection de la fonction insensible à la casse (afin que les utilisateurs puissent saisir pierre, pIERRE, pirrE ou toute autre variante).
+
+// Remarque importante : On souhaite retourner les résultats de cet appel de fonction.
+
+// function playRound(playerSelection, computerSelection) {
+//   // Le code
+// }
+
+// const playerSelection = 'pierre';
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+// Écrire une NOUVELLE fonction appelée game(). Appeler la fonction playRound à l'intérieur de celle-ci pour jouer à un jeu de 5 tours qui enregistre le score et signale un gagnant ou un perdant à la fin.
+// À ce stade, on utilisera console.log() pour afficher les résultats de chaque tour et le gagnant à la fin.
+
+// Utiliser prompt() pour obtenir l'entrée de l'utilisateur
